8fffd66c9405498a46c2e5d7db0ed19aa84ef091
zh dk <zhdk@zhdeMacBook-Pro.local>
2017-08-21 14:37:54 +0800

    Initial Commit

---
+++ 步进器和分栏控件/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  步进器和分栏控件
+//
+//  Created by zh dk on 2017/8/21.
+//  Copyright © 2017年 zh dk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
